6f1f5166c91c8d8096f9951dbca6e135888be4d8
Michael Null <michael.null@bpmw.net>
2015-07-23 16:08:54 -0500

    minor changes

--- WebApplication1/WebApplication1/Models/People.cs
+++ WebApplication1/WebApplication1/Models/People.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
 namespace WebApplication1.Models
 {
     public class People
-    {
+    {   [DisplayName("First Name")]
         public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        [DisplayName("email address")]
         public string EMail { get; set; }
+
+        [DisplayName("Full Name")]
         public string FullName()
         {
             {
