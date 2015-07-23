0b7d3aa06ef4d1effc8a86062686fc7d150f3919
Scott Ferguson <esfergus@gmail.com>
2015-07-23 11:36:20 -0500

    Added view page and download CSV page.

--- Week4.Day3/Week4.Day3/Controllers/UsersController.cs
+++ Week4.Day3/Week4.Day3/Controllers/UsersController.cs
@@ -21,26 +21,27 @@ namespace Week4.Day3.Controllers
         public ActionResult Parse()
         {
             var engine = new FileHelperEngine<User>();
-            var result = engine.ReadFileAsList("C:/TIY/Projects/Week-4-Day-3/Week4.Day3/Week4.Day3/Files/users.txt");
+            List<User> result = engine.ReadFileAsList("C:/TIY/Projects/Week-4-Day-3/Week4.Day3/Week4.Day3/Files/users.txt");
             StringBuilder lines = new StringBuilder();
             lines.AppendLine("First Name, Last Name, e-mail address");
             foreach (User u in result)
             {
                 lines.AppendLine(u.FirstName + ", " + u.LastName + ", " + u.Email);
-            }
-            byte[] myUsers = Encoding.Default.GetBytes(lines.ToString());
-            return File(myUsers,"text/html","My_Users.csv");
+            }           
+            Session["CSVFilePackage"] = lines;
+            return View(result);
         }
 
 
         // POST: Users/ViewUsers
-        [HttpPost]
         public ActionResult ViewUsers()
         {
             try
             {
-                // TODO: Add insert logic here
-                return View();
+                // TODO: Add insert logic here   
+                var lines = Session["CSVFilePackage"];
+                byte[] myUsers = Encoding.Default.GetBytes(lines.ToString());
+                return File(myUsers, "text/html", "My_Users.csv");
             }
             catch
             {
